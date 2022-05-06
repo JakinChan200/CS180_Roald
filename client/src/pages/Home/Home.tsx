bbf006587f809132984e9aa580378de92c883096
Jakin Chan <JakinChan200@gmail.com>
2022-05-05 21:36:51 -0700

    Saving Progress 3

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -10,62 +10,39 @@ import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
 
 interface CountryNumVids {
   id: string;
-  short: string;
   value: null | number;
 }
-
-// export const Country: React.FC<CountryProps> = ({ country }) => {
-//   const [loading, setLoading] = React.useState<boolean>(false);
-//   const [error, setError] = React.useState<string>("");
-//   const [catResults, setCatResults] = React.useState<Video[]>([]);
-//   const [genResults, setGenResults] = React.useState<Video[]>([]);
-//   const expResults = React.useContext(UserContext).videos;
-//   const [avgResults, setAvgResults] = React.useState<Average[]>(
-//     [
-//       { name: "Average Comments", short: "avg_comments", value: null },
-//       { name: "Average Likes", short: "avg_likes", value: null },
-//       { name: "Average Dislikes", short: "avg_dislikes", value: null },
-//       { name: "Average Views", short: "avg_views", value: null },
-//     ].sort((a, b) => a.name.localeCompare(b.name, "en"))
-//   );
-//   const categories = getCategories(country);
-
-//   const getResult = (value: string) => {
-//     axios
-//       .get(`${BACKEND_URL}/countries/${country}?id=${value}`)
-//       .then((res) => setCatResults(res.data.videos))
-//       .catch((e) => {
-//         setError("Error fetching video data.");
-//       });
-// };
-
-// const routes = [
-//   { name: "/", Component: <Home /> },
-//   ...countries.map((country: string) => ({
-//     name: country,
-//     Component: <Country country={country} key={country} />,
-//   })),
-// ].sort((a, b) => a.name.localeCompare(b.name, "en"));
 
 export const Home = () => {
   const [enter, setEnter] = React.useState<boolean>(false);
   const [error, setError] = React.useState<string>("");
   const [numCountryVideos, setNumCountryVideos] = React.useState<CountryNumVids[]>(
     [
-      {id: "", short: "video_count", value: null},
+      {id: "", value: null},
     ]
   );
   
 
-  const getResult = (value: string) => {
+  const getNumVideos = (value: string) => {
     axios
-      .get(`${BACKEND_URL}/countries/${countries}`)
-      .then((res) => setNumCountryVideos(res.data.num_videos))
+      .get(`${BACKEND_URL}/countries/${value}`)
+      .then((res) => {
+        setNumCountryVideos((prev) =>
+          prev
+            .map(() => ({
+              id: value,
+              value: res.data.num_videos,
+            }))
+        );
+      })
       .catch((e) => {
         setError("Error fetching video data.");
     });
   };
-  console.log(getResult)
+
+  for(let i = 0; i < countries.length; i++){
+    getNumVideos(countries[i]);
+  }
 
   return (
     <div className="homeContainer">
@@ -85,12 +62,12 @@ export const Home = () => {
             animate={{ y: -50 }}
             transition={{ type: "spring", duration: 1 }}
           >
-            <PieChart
-              results={numCountryVideos.sort()        
-                .map((CountryNumVids) => ({
-                  id: CountryNumVids.id,
-                  value: CountryNumVids.value,
-                }))}
+            <PieChart results={numCountryVideos}
+              // results={numCountryVideos.sort()        
+              //   .map((CountryNumVids) => ({
+              //     id: CountryNumVids.id,
+              //     value: CountryNumVids.value,
+              //   }))}
             />
           </motion.div>
         </>
